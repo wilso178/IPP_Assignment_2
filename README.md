f3ca5ea5d3a3163723540f0de25bc4e6faebf03a
Michael <wilso178@sheridancollege.ca>
2020-09-21 20:40:40 -0400

    Add README and print_name files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hello <input NAME>
+Type in your name after the words "Enter your name:" when prompted.  The script will then display your name in a sentence.
--- print_name.py
+++ print_name.py
@@ -3,4 +3,4 @@ Input example #1
 """
 NAME = input('Enter your name: ')
 
-print('Hello', NAME + '. Welcome to Pyton Intermediate 2. The datatype is a string:', type(NAME))
+print('Hello', NAME + '. Welcome to Python Intermediate 2. The datatype is a string:', type(NAME))
